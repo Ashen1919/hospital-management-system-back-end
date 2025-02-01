e6bd4267480a5d7d1f6d23a4b963e9cbae6eeff7
Ashen1919 <2303S0834@saegis.ac.lk>
2025-02-02 00:22:13 +0530

    create user model

---
+++ models/user.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose'
+
+const userSchema = mongoose.Schema({
+    email : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    firstName : {
+        type : String,
+        required : true
+    },
+    lastName: {
+        type : String,
+        required : true
+    },
+    type: {
+        type: String,
+        required: false,
+        default: "patient"
+    },
+    mobile: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    emailVerified: {
+        type: Boolean,
+        required: false,
+        default: false
+    },
+    profileImage: {
+        type: String,
+        default: "https://cloud.appwrite.io/v1/storage/buckets/672a1e700037c646954e/files/674578c80028b7645a44/view?project=672a1dc2000b4396bb7d&project=672a1dc2000b4396bb7d&mode=admin"
+    }
+});
+
+const User = mongoose.model("user", userSchema);
+export default User;
